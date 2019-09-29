e5fb602bfa58d31b2131149271b0cc5e9dd8046d
adhe <you@example.com>
2019-09-29 22:59:03 +0700

    login

---
+++ app/src/main/java/dev7/id/pluginappsclient/LoginActivity.kt
@@ -0,0 +1,12 @@
+package dev7.id.pluginappsclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
