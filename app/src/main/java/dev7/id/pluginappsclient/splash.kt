4bd403fa88e77189c6489e5562eda3cc77a743ae
setiywan12 <yayangsetiyawan0@gmail.com>
2019-11-12 21:19:11 +0700

    Kangen Commit Gaes

---
+++ app/src/main/java/dev7/id/pluginappsclient/splash.kt
@@ -0,0 +1,12 @@
+package dev7.id.pluginappsclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+    }
+}
