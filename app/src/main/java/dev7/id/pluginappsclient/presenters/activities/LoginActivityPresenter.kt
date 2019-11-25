74e3ded9ada5a8db07341aba707d879a9e1b0dd0
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-25 19:08:52 +0700

    Tinggal panggil api login

--- app/src/main/java/dev7/id/pluginappsclient/presenters/activities/LoginActivityPresenter.kt
+++ app/src/main/java/dev7/id/pluginappsclient/presenters/activities/LoginActivityPresenter.kt
@@ -1,9 +1,20 @@
 package dev7.id.pluginappsclient.presenters.activities
 
 import dev7.id.pluginappsclient.contracts.activities.LoginActivityContract
+import dev7.id.pluginappsclient.models.User
 import dev7.id.pluginappsclient.utilities.PluginUtils
+import dev7.id.pluginappsclient.webservices.PluginAPI
+import dev7.id.pluginappsclient.webservices.WrappedListResponse
+import dev7.id.pluginappsclient.webservices.WrappedResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class LoginActivityPresenter(var view : LoginActivityContract.View?) : LoginActivityContract.Interactor {
+class LoginActivityPresenter(view : LoginActivityContract.View?) : LoginActivityContract.Interactor {
+
+    private var view : LoginActivityContract.View? = view
+    private var api = PluginAPI.instance()
+
     override fun validate(id: String, pass: String) : Boolean{
         view?.idError(null)
         view?.passwordError(null)
@@ -18,7 +29,21 @@ class LoginActivityPresenter(var view : LoginActivityContract.View?) : LoginActi
         return true
     }
 
-    override fun login(id: String, pass: String) {
+    override fun login(member_id: String, password: String) {
+        view?.isLoading(state = true)
+        api.login(member_id, password).enqueue(object : Callback<WrappedResponse<User>>{
+            override fun onFailure(call: Call<WrappedResponse<User>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(
+                call: Call<WrappedResponse<User>>,
+                response: Response<WrappedResponse<User>>
+            ) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
 
     }
 
--- app/src/main/java/dev7/id/pluginappsclient/webservices/PluginAPI.kt
+++ app/src/main/java/dev7/id/pluginappsclient/webservices/PluginAPI.kt
@@ -7,7 +7,10 @@ import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 import java.util.concurrent.TimeUnit
 
 class PluginAPI {
@@ -32,7 +35,20 @@ class PluginAPI {
 interface PluginAPIService {
     @GET("api/users/")
     fun users() : Call<WrappedListResponse<User>>
+
+    @FormUrlEncoded
+    @POST("api/login/")
+    fun login(@Field("member_id") member_id : String , @Field("password") password : String) : Call<WrappedResponse<User>>
 }
+
+interface PluginEvent {
+
+}
+
+//interface PluginAPIService {
+//    @GET("api/users/")
+//    fun users() : Call<WrappedListResponse<User>>
+//}
 
 
 
